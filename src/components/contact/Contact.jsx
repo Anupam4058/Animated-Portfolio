eb805f4ac82a3320b7753ddadd1c623968abfbfd
ANUPAM KUMAR SINGH <anupamkumar46511@gmail.com>
2025-10-08 22:47:30 +0530

    #

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -130,7 +130,7 @@ const Contact = () => {
           <circle cx="4" cy="4" r="2" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       ),
-      link: "https://linkedin.com/in/anupam-kumar-singh"
+      link: "https://linkedin.com/in/anupam-kumar-singh-0b647224a"
     },
     {
       name: "Twitter",
@@ -139,7 +139,7 @@ const Contact = () => {
           <path d="M23 3C22.0424 3.67548 20.9821 4.19211 19.86 4.53C19.2577 3.83751 18.4573 3.34669 17.567 3.12393C16.6767 2.90116 15.7395 2.95718 14.8821 3.28445C14.0247 3.61172 13.2884 4.19439 12.773 4.95372C12.2575 5.71305 11.9877 6.61232 12 7.53V8.53C10.2426 8.57557 8.50127 8.18581 6.93101 7.39525C5.36074 6.60469 4.01032 5.43866 3 4C3 4 -1 13 8 17C5.94053 18.398 3.48716 19.099 1 19C10 24 21 19 21 7.5C20.9991 7.22145 20.9723 6.94359 20.92 6.67C21.9406 5.66349 22.6608 4.39271 23 3V3Z" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
       ),
-      link: "https://twitter.com/anupamkumar"
+      link: "https://x.com/Anupam4058"
     }
   ];
 
