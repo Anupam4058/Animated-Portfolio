be46c0d28018de61f7b317d3c2ebd811934c30f4
ANUPAM KUMAR SINGH <anupamkumar46511@gmail.com>
2026-01-27 00:44:35 +0530

    Fixed some UI flaws

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./app.scss";
 import Contact from "./components/contact/Contact";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
-import Parallax from "./components/parallax/Parallax";
 import Portfolio from "./components/portfolio/Portfolio";
 import Services from "./components/services/Services";
 import About from "./components/about/About";
@@ -56,17 +55,13 @@ const App = () => {
             Skip to main content
           </a>
           
-          
-          
-          
-          
-          
+          {/* Fixed Navbar - Always visible at top */}
+          <ErrorBoundary>
+            <Navbar />
+          </ErrorBoundary>
           
           {/* Hero Section */}
-          <section id="Homepage" className="section">
-            <ErrorBoundary>
-              <Navbar />
-            </ErrorBoundary>
+          <section id="Homepage" className="section hero-section">
             <ErrorBoundary>
               <Hero />
             </ErrorBoundary>
@@ -75,43 +70,28 @@ const App = () => {
           {/* Main Content */}
           <main id="main-content">
             {/* About Section */}
-            <section id="About" className="section" aria-labelledby="about-heading">
-              <ErrorBoundary>
-                <Parallax type="about" />
-              </ErrorBoundary>
-            </section>
-            <section className="section">
+            <section id="About" className="section content-section" aria-labelledby="about-heading">
               <ErrorBoundary>
                 <About />
               </ErrorBoundary>
             </section>
             
             {/* Services Section */}
-            <section id="Services" className="section" aria-labelledby="services-heading">
-              <ErrorBoundary>
-                <Parallax type="services" />
-              </ErrorBoundary>
-            </section>
-            <section className="section">
+            <section id="Services" className="section content-section" aria-labelledby="services-heading">
               <ErrorBoundary>
                 <Services />
               </ErrorBoundary>
             </section>
             
             {/* Portfolio Section */}
-            <section id="Portfolio" className="section" aria-labelledby="portfolio-heading">
-              <ErrorBoundary>
-                <Parallax type="portfolio" />
-              </ErrorBoundary>
-            </section>
-            <section className="section">
+            <section id="Portfolio" className="section content-section" aria-labelledby="portfolio-heading">
               <ErrorBoundary>
                 <Portfolio />
               </ErrorBoundary>
             </section>
             
             {/* Contact Section */}
-            <section id="Contact" className="section" aria-labelledby="contact-heading">
+            <section id="Contact" className="section content-section" aria-labelledby="contact-heading">
               <ErrorBoundary>
                 <Contact />
               </ErrorBoundary>
